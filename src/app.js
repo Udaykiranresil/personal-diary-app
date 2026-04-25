32a1b4379cbfc16b398a44b94b67da5656f9e4c6
K. Uday kiran Achari <udaykiran172008@gmail.com>
2026-04-25 19:07:14 +0530

    fixed localhost issue

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json())
 
 app.use(session({
-    secret: 'process.env.SESSION_SECRET',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
