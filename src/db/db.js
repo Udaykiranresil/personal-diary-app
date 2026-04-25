14ca6f524925b2b7120600f0a667487c563be5b3
K. Uday kiran Achari <udaykiran172008@gmail.com>
2026-04-25 17:38:03 +0530

    Secured mongo commit

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 async function connectDB(){
     try {
-        await mongoose.connect("mongodb+srv://yt:D4qn8DQxK72QFl9U@cluster0.ag87hhr.mongodb.net/halley")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('Database is connected')
     } catch (error) {
         console.log(error)
